62a637f923f0fcb8533ce254296f1140129a5bc3
Ignacio Fuentes <ignaciofuentes@outlook.com>
2025-05-30 11:09:24 -0400

    adding yaml

--- AppSyncService.cs
+++ AppSyncService.cs
@@ -1,11 +1,8 @@
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
+
 
 // Models
-
 public class GraphQLResponse<T>
 {
     public T Data { get; set; }
